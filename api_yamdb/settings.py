1f9b871a4b5401d64ae646584b492e50098f1f60
ilyukevich <9009901@gmail.com>
2021-02-20 19:54:45 +0300

    v17

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['84.252.129.156', 'localhost',]
 
 
 # Application definition
